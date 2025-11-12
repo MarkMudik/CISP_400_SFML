6653156f15348a05813250dc545ef689770bcee6
Your Name <you@example.com>
2025-11-12 11:18:11 -0800

    Second

--- Chapter13/code/ZombieArena.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "Zombie.h"
-using namespace sf;
-int createBackground(VertexArray& rVA, IntRect arena);
-Zombie* createHorde(int numZombies, IntRect arena);
